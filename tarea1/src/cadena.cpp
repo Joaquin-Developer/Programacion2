f115e67f818be8a89583b541608f24f7123b29f5
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-03-18 02:10:54 -0300

    errores

--- tarea1/src/cadena.cpp
+++ tarea1/src/cadena.cpp
@@ -5,18 +5,12 @@
 // los elementos se mantienen entre 0 y cantidad - 1, incluidos
 struct _rep_cadena
 {
-    // TInfo info;
-    // _rep_cadena *siguiente;
     nat longitud;
     TInfo lista[L];
 };
 
 TCadena crearCadena()
 {
-    // TCadena cadena = new _rep_cadena;
-    // cadena->info = NULL;
-    // cadena->siguiente = NULL;
-    // return cadena;
     TCadena cadena = new _rep_cadena;
     (*cadena).longitud = 0;
     return cadena;
@@ -30,16 +24,6 @@ void liberarCadena(TCadena cad) {
 nat cantidadEnCadena(TCadena cad)
 {
     return (*cad).longitud;
-    // int cont = 0;
-    // TCadena aux = cad;
-
-    // while (aux != NULL)
-    // {
-    //     cont++; // incrementar en 1
-    //     aux = aux->siguiente;
-    // }
-
-    // return cont;
 }
 
 bool estaEnCadena(nat natural, TCadena cad)
@@ -51,15 +35,6 @@ bool estaEnCadena(nat natural, TCadena cad)
         if (natInfo((*cad).lista[i]) == natural)
             estaEnCadena = true;
     return estaEnCadena;
-    // bool estaEnCadena = false;
-    // TCadena aux = cad;
-    // while (aux != NULL || !estaEnCadena)
-    // {
-    //     if (natInfo(aux->info) == natural)
-    //         estaEnCadena = true;
-    //     aux = aux->siguiente;
-    // }
-    // return estaEnCadena;
 }
 
 TCadena insertarAlInicio(nat natural, double real, TCadena cad)
@@ -76,35 +51,10 @@ TCadena insertarAlInicio(nat natural, double real, TCadena cad)
         (*cad).lista[0] = info;
     }
     return cad;
-
-    // // creo la nueva cadena:
-    // TCadena cadena = crearCadena();
-    // // creo el info, elemento de la cadena:
-    // TInfo info = crearInfo(natural, real);
-    // // asigno al campo info de cadena, el info que creé:
-    // cadena->info = info;
-    // // cadena auxiliar, con el primer elemento que había:
-    // TCadena aux = cad;
-    // // inserto:
-    // cad = cadena;
-    // cadena->siguiente = aux;
-    // return cad;
 }
 
 TInfo infoCadena(nat natural, TCadena cad)
 {
-    // TCadena aux = cad;
-    // TInfo info = NULL;
-
-    // while (aux != NULL || info == NULL)
-    // {
-    //     if (natInfo(aux->info) == natural)
-    //         info = aux->info;
-    //     aux = aux->siguiente;
-    // }
-
-    // return info;
-
     TInfo tinfo = NULL;
     nat i = 0;
 
@@ -117,31 +67,35 @@ TInfo infoCadena(nat natural, TCadena cad)
 
 TCadena removerDeCadena(nat natural, TCadena cad)
 {
-    return NULL;
-    // TCadena aux = cad;
-    // TInfo info = NULL;
+    int indiceBorrar = -1;
+    nat cantidad = cantidadEnCadena(cad);
 
-    // while (aux != NULL || info == NULL)
-    // {
-    //     if (natInfo(aux->info) == natural)
-    //         info = aux->info;
-    //     aux = aux->siguiente;
+    nat i = 0;
+    while (i < cantidad || indiceBorrar != -1)
+    {
+        if (natInfo((*cad).lista[i]) == natural)
+            indiceBorrar = i;
+        i++;
+    }
+
+    // 1 2 3 4 5 6
+    // i = 2
+    // 1 2 4 5 6
+
+    if (indiceBorrar != -1)
+    {
+        for (i = indiceBorrar; i < cantidad - 1; i++)
+        {
+            (*cad).lista[i] = (*cad).lista[i + 1];
+        }
+    }
+    (*cad).longitud--;
+    return cad;
 }
 
 void imprimirCadena(TCadena cad)
 {
     nat cantidad = cantidadEnCadena(cad);
-    // TCadena aux = cad;
-
-    // if (cantidadEnCadena(cad) != 0)
-    // {
-    //     while (aux != NULL)
-    //     {
-    //         printf("%s", infoATexto(cad->info));
-    //         aux = aux->siguiente;
-    //     }
-    // }
-    // printf("\n");
 
     if (cantidad != 0)
         for (nat i = 0; i < cantidad; i++)
--- tarea1/src/colCadenas.cpp
+++ tarea1/src/colCadenas.cpp
@@ -1,14 +1,21 @@
 /* 5124352 */
 
 #include "../include/colCadenas.h"
+#include "../include/cadena.h"
 
 struct _rep_colCadenas
 {
+    TCadena lista[CANT_CADS];
 };
 
 TColCadenas crearColCadenas()
 {
-  return NULL;
+    TColCadenas col = new _rep_colCadenas;
+
+    for (int i = 0; i < CANT_CADS; i++)
+        (*col).lista[i] = crearCadena();
+
+    return col;
 }
 
 /* en siguientes tareas
@@ -17,25 +24,35 @@ void liberarColCadenas(TColCadenas col) {
 */
 nat cantidadColCadenas(nat pos, TColCadenas col)
 {
-  return 0;
+    return cantidadEnCadena((*col).lista[pos]);
 }
 
 bool estaEnColCadenas(nat natural, nat pos, TColCadenas col)
 {
-  return false;
+    bool esta = false;
+    int i = 0;
+
+    while (i < CANT_CADS && !esta)
+    {
+        esta = estaEnCadena(natural, (*col).lista[i]);
+        i++;
+    }
+    return esta;
 }
 
 TColCadenas insertarEnColCadenas(nat natural, double real, nat pos, TColCadenas col)
 {
-  return NULL;
+    insertarAlInicio(natural, real, (*col).lista[pos]);
+    return col;
 }
 
 TInfo infoEnColCadenas(nat natural, nat pos, TColCadenas col)
 {
-  return NULL;
+    return infoCadena(natural, (*col).lista[pos]);
 }
 
 TColCadenas removerDeColCadenas(nat natural, nat pos, TColCadenas col)
 {
-  return NULL;
+    removerDeCadena(natural, (*col).lista[pos]);
+    return col;
 }
