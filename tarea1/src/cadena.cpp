4b03648b06cde2f522db08c5683b912919c70f00
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-03-17 23:44:19 -0300

    cambio lista enlazada a array

--- tarea1/src/cadena.cpp
+++ tarea1/src/cadena.cpp
@@ -5,12 +5,18 @@
 // los elementos se mantienen entre 0 y cantidad - 1, incluidos
 struct _rep_cadena
 {
+    // TInfo info;
+    // _rep_cadena *siguiente;
     nat longitud;
     TInfo lista[L];
 };
 
 TCadena crearCadena()
 {
+    // TCadena cadena = new _rep_cadena;
+    // cadena->info = NULL;
+    // cadena->siguiente = NULL;
+    // return cadena;
     TCadena cadena = new _rep_cadena;
     (*cadena).longitud = 0;
     return cadena;
@@ -24,6 +30,16 @@ void liberarCadena(TCadena cad) {
 nat cantidadEnCadena(TCadena cad)
 {
     return (*cad).longitud;
+    // int cont = 0;
+    // TCadena aux = cad;
+
+    // while (aux != NULL)
+    // {
+    //     cont++; // incrementar en 1
+    //     aux = aux->siguiente;
+    // }
+
+    // return cont;
 }
 
 bool estaEnCadena(nat natural, TCadena cad)
@@ -34,17 +50,61 @@ bool estaEnCadena(nat natural, TCadena cad)
     while (i < cantidadEnCadena(cad) || estaEnCadena)
         if (natInfo((*cad).lista[i]) == natural)
             estaEnCadena = true;
-
     return estaEnCadena;
+    // bool estaEnCadena = false;
+    // TCadena aux = cad;
+    // while (aux != NULL || !estaEnCadena)
+    // {
+    //     if (natInfo(aux->info) == natural)
+    //         estaEnCadena = true;
+    //     aux = aux->siguiente;
+    // }
+    // return estaEnCadena;
 }
 
 TCadena insertarAlInicio(nat natural, double real, TCadena cad)
 {
-    return NULL;
+    if ((cantidadEnCadena(cad) < L))
+    {
+        (*cad).longitud++;
+        for (nat i = 0; i < cantidadEnCadena(cad); i++)
+        {
+            (*cad).lista[i + 1] = (*cad).lista[i];
+        }
+        // Tinfo a insertar:
+        TInfo info = crearInfo(natural, real);
+        (*cad).lista[0] = info;
+    }
+    return cad;
+
+    // // creo la nueva cadena:
+    // TCadena cadena = crearCadena();
+    // // creo el info, elemento de la cadena:
+    // TInfo info = crearInfo(natural, real);
+    // // asigno al campo info de cadena, el info que creé:
+    // cadena->info = info;
+    // // cadena auxiliar, con el primer elemento que había:
+    // TCadena aux = cad;
+    // // inserto:
+    // cad = cadena;
+    // cadena->siguiente = aux;
+    // return cad;
 }
 
 TInfo infoCadena(nat natural, TCadena cad)
 {
+    // TCadena aux = cad;
+    // TInfo info = NULL;
+
+    // while (aux != NULL || info == NULL)
+    // {
+    //     if (natInfo(aux->info) == natural)
+    //         info = aux->info;
+    //     aux = aux->siguiente;
+    // }
+
+    // return info;
+
     TInfo tinfo = NULL;
     nat i = 0;
 
@@ -58,15 +118,34 @@ TInfo infoCadena(nat natural, TCadena cad)
 TCadena removerDeCadena(nat natural, TCadena cad)
 {
     return NULL;
+    // TCadena aux = cad;
+    // TInfo info = NULL;
+
+    // while (aux != NULL || info == NULL)
+    // {
+    //     if (natInfo(aux->info) == natural)
+    //         info = aux->info;
+    //     aux = aux->siguiente;
 }
 
 void imprimirCadena(TCadena cad)
 {
-    int cantidad = cantidadEnCadena(cad);
+    nat cantidad = cantidadEnCadena(cad);
+    // TCadena aux = cad;
+
+    // if (cantidadEnCadena(cad) != 0)
+    // {
+    //     while (aux != NULL)
+    //     {
+    //         printf("%s", infoATexto(cad->info));
+    //         aux = aux->siguiente;
+    //     }
+    // }
+    // printf("\n");
 
     if (cantidad != 0)
-        for (int i = 0; i < cantidad; i++)
-            printf(infoATexto((*cad).lista[i]));
+        for (nat i = 0; i < cantidad; i++)
+            imprimirInfo((*cad).lista[i]);
 
     printf("\n");
 }
