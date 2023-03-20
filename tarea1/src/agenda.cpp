90fdca563f7bba9ce2f4a1e656c74beac608b86b
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2023-03-20 15:14:37 -0300

    entrega 1version

--- tarea1/src/agenda.cpp
+++ tarea1/src/agenda.cpp
@@ -29,8 +29,10 @@ void agregarEnAgenda(TAgenda &agenda, TEvento evento)
     // Agregar - caso que este vacia:
     if (tamanioAgenda(agenda) == 0)
     {
-        (*agenda).tope++;
-        (*agenda).lista[0] = evento;
+        // (*agenda).tope++;
+        // (*agenda).lista[0] = evento;
+        agenda->tope++;
+        agenda->lista[0] = evento;
         return;
     }
     // Agregar - resto de los casos:
@@ -47,7 +49,7 @@ void agregarEnAgenda(TAgenda &agenda, TEvento evento)
         while (indiceInsertar == -1 && i < tamanioAgenda(agenda))
         {
             // si fecha actual >= fecha insertar, entonces inserto en indice actual.
-            if (compararTFechas(fechaTEvento((*agenda).lista[0]), fechaTEvento(evento)) >= 0)
+            if (compararTFechas(fechaTEvento((*agenda).lista[i]), fechaTEvento(evento)) >= 0)
                 indiceInsertar = i;
             i++;
         }
@@ -123,39 +125,68 @@ int obtenerIndiceEnArray(TAgenda agenda, int id)
 
 void posponerEnAgenda(TAgenda &agenda, int id, nat n)
 {
-
-    TEvento evento = obtenerDeAgenda(agenda, id);
-
     int indiceActual = obtenerIndiceEnArray(agenda, id);
+    TEvento evento = agenda->lista[indiceActual];
 
-    // calculo el índice de donde voy a tener que insertarlo:
-    int indiceInsertar = -1;
-
-    // si fecha evento a mover es mayor a ultima fecha, entonces muevo al final.
-    if (compararTFechas(fechaTEvento(evento), fechaTEvento(agenda->lista[tamanioAgenda(agenda) - 1])) == 1)
-        indiceInsertar = tamanioAgenda(agenda) - 1;
-    else
-    {
-        nat i = indiceActual;
-        while (indiceInsertar == -1 && i < tamanioAgenda(agenda))
-        {
-            // si fecha actual >= fecha evento mover, entonces muevo a indice actual.
-            if (compararTFechas(fechaTEvento(agenda->lista[i]), fechaTEvento(evento)) >= 0)
-                indiceInsertar = i;
-            i++;
-        }
-        if (indiceInsertar == -1)
-            indiceInsertar = tamanioAgenda(agenda) - 1;
-    }
-
-    for (int i = indiceActual; i < indiceInsertar; i++)
-    {
-        agenda->lista[i] = agenda->lista[i + 1];
-    }
+    removerDeAgenda(agenda, id);
 
     posponerTEvento(evento, n);
-    agenda->lista[indiceInsertar] = evento;
+    agregarEnAgenda(agenda, evento);
 }
+
+// void posponerEnAgenda(TAgenda &agenda, int id, nat n)
+// {
+//     int indiceActual = obtenerIndiceEnArray(agenda, id);
+//     TEvento evento = agenda->lista[indiceActual];
+
+//     // TEvento nuevoEvento = crearTEvento(id, evento->descripcion, fechaTEvento(evento));
+
+//     // calculo el índice de donde voy a tener que insertarlo:
+//     posponerTEvento(evento, n);
+//     int indiceInsertar = -1;
+
+//     // si fecha evento a mover es mayor a ultima fecha, entonces muevo al final.
+//     if (compararTFechas(fechaTEvento(evento), fechaTEvento(agenda->lista[tamanioAgenda(agenda) - 1])) == 1)
+//         indiceInsertar = tamanioAgenda(agenda) - 1;
+//     else
+//     {
+//         nat i = indiceActual;
+//         while (indiceInsertar == -1 && i < tamanioAgenda(agenda))
+//         {
+//             // si fecha actual >= fecha evento mover, entonces muevo a indice actual.
+//             if (compararTFechas(fechaTEvento(agenda->lista[i]), fechaTEvento(evento)) >= 0)
+//                 indiceInsertar = i - 1;
+//             i++;
+//         }
+//     }
+//     /* Hasta acá funciona bien, se calcula el indice a mover! */
+
+//     // liberarTEvento(agenda->lista[2]);
+//     // agenda->lista[2] = agenda->lista[0];
+//     // liberarTEvento(agenda->lista[0]);
+//     // agenda->lista[0] = aux;
+
+//     // TEvento aux = agenda->lista[indiceActual + 1];
+
+//     // for (int i = indiceActual + 1; i < indiceInsertar; i++)
+//     // {
+//     //     liberarTEvento(agenda->lista[i]);
+//     //     agenda->lista[i] = agenda->lista[i + 1];
+//     //     // agenda->lista[i] = agenda->lista[i + 1];
+//     // }
+//     // liberarTEvento(agenda->lista[indiceInsertar]);
+//     // agenda->lista[indiceInsertar] = agenda->lista[indiceActual];
+
+//     // agenda->lista[indiceActual] = aux;
+//     // liberarTEvento(agenda->lista[indiceActual]);
+//     // agenda->lista[indiceActual] = aux;
+
+//     // agenda->lista[indiceInsertar] = evento;
+
+//     // (*agenda).lista[indiceInsertar] = evento;
+//     // posponerTEvento((*agenda).lista[indiceInsertar], n);
+//     // agenda->lista[indiceInsertar] = aux;
+// }
 
 void imprimirEventosFecha(TAgenda agenda, TFecha fecha)
 {
@@ -201,8 +232,11 @@ void removerDeAgenda(TAgenda &agenda, int id)
 {
     int indiceBorrar = obtenerIndiceEnArray(agenda, id);
 
-    for (nat i = indiceBorrar; i < tamanioAgenda(agenda); i--)
-        (*agenda).lista[i] = (*agenda).lista[i + 1];
+    for (nat i = indiceBorrar; i < tamanioAgenda(agenda) - 1; i--)
+    {
+        liberarTEvento(agenda->lista[i]);
+        agenda->lista[i] = agenda->lista[i + 1];
+    }
 
     (*agenda).tope--;
 }
