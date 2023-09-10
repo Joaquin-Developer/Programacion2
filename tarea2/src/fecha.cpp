e6b8f6caa8361e70b02645ce120a3881b924fbab
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2023-09-10 19:14:52 -0300

    tarea2 - 2023S2

---
+++ tarea2/src/fecha.cpp
@@ -0,0 +1,32 @@
+#include "../include/fecha.h"
+
+// Estructura para almacenar una fecha con día, mes y año
+struct rep_fecha {
+};
+
+// Función para crear un nuevo objeto de tipo TFecha con una fecha dada
+TFecha crearTFecha(nat dia, nat mes, nat anio) {
+    return NULL;
+}
+
+// Función para liberar un objeto de tipo TFecha previamente creado
+void liberarTFecha(TFecha &fecha) {
+}
+
+// Función para imprimir una fecha representada por un objeto de tipo TFecha
+void imprimirTFecha(TFecha fecha) {
+}
+
+// Función para aumentar una fecha en una cantidad dada de días
+void aumentarTFecha(TFecha &fecha, nat dias) {
+}
+
+// Función para comparar dos fechas representadas por objetos de tipo TFecha
+int compararTFechas(TFecha fecha1, TFecha fecha2) {
+    return 0;
+}
+
+// Retorna una copia de la fecha
+TFecha copiarTFecha(TFecha fecha) {
+    return NULL;
+}
---
+++ tarea2/src/jugada.cpp
@@ -0,0 +1,38 @@
+#include "../include/jugada.h"
+
+// Define la estructura Jugada
+struct rep_jugada {
+};
+
+// Crea una jugada con los valores dados
+TJugada crearTJugada(int numero, Movimiento mov1, Movimiento mov2, Movimiento mov3, Movimiento mov4){
+    return NULL;
+}
+
+// Función que retorna el numero de la jugada
+int numeroTJugada(TJugada jugada){
+    return 0;
+}
+
+// Función que retorna los movimientos de una jugada
+Movimiento * movimientoTJugada(TJugada jugada) {
+    return NULL;
+}
+
+// Devuelve true si y solo si la jugada tiene el movimiento mov en la posicion pos
+bool tieneMovimientoEnPosTJugada(TJugada jugada, int pos, Movimiento mov){
+    return false;
+}
+
+// Imprime la jugada
+void imprimirTJugada(TJugada jugada) {
+}
+
+// Libera la memoria asignada para un jugada
+void liberarTJugada(TJugada &jugada) {
+}
+
+// Retorna una copia de la jugada
+TJugada copiarTJugada(TJugada jugada) {
+    return NULL;
+}
