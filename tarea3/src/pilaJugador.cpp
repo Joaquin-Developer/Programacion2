8b6aef9b2b956b110c53975681dc3aad07661da1
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2023-10-15 18:12:15 -0300

    Pila

--- tarea3/src/pilaJugador.cpp
+++ tarea3/src/pilaJugador.cpp
@@ -1,29 +1,67 @@
 #include "../include/pilaJugador.h"
 
-struct rep_pilaJugador {
-
+struct rep_nodo_pila
+{
+  TJugador jugador;
+  rep_nodo_pila *siguiente;
 };
 
-TPilaJugador crearTPilaJugador() {
-  return NULL;
+typedef rep_nodo_pila *TNodoPila;
+
+struct rep_pilaJugador
+{
+  nat cantidad;   // cantidad (length) de la Pila
+  TNodoPila cima; // puntero al primer elemento de la pila
+};
+
+TPilaJugador crearTPilaJugador()
+{
+  TPilaJugador pila = new rep_pilaJugador;
+  pila->cantidad = 0;
+  pila->cima = NULL;
+  return pila;
 }
 
-void liberarTPilaJugador(TPilaJugador &p) {
-
+void liberarTPilaJugador(TPilaJugador &p)
+{
+  while (p->cantidad != 0)
+  {
+    TNodoPila borrar = p->cima;
+    p->cima = p->cima->siguiente;
+    p->cantidad--;
+    liberarTJugador(borrar->jugador);
+    delete borrar;
+    borrar = NULL;
+  }
+  delete p;
+  p = NULL;
 }
 
-nat cantidadEnTPilaJugador(TPilaJugador p) { 
-  return 0;
+nat cantidadEnTPilaJugador(TPilaJugador p)
+{
+  return p->cantidad;
 }
 
-void apilarEnTPilaJugador(TPilaJugador &p, TJugador jugador) {
-
+void apilarEnTPilaJugador(TPilaJugador &p, TJugador jugador)
+{
+  TNodoPila insertar = new rep_nodo_pila;
+  insertar->jugador = copiarTJugador(jugador);
+  insertar->siguiente = p->cima;
+  p->cima = insertar;
+  p->cantidad++;
 }
 
-TJugador cimaDeTPilaJugador(TPilaJugador p) { 
-  return NULL;
+TJugador cimaDeTPilaJugador(TPilaJugador p)
+{
+  return p->cima->jugador;
 }
 
-void desapilarDeTPilaJugador(TPilaJugador &p) {
-
+void desapilarDeTPilaJugador(TPilaJugador &p)
+{
+  TNodoPila borrar = p->cima;
+  p->cima = p->cima->siguiente;
+  p->cantidad--;
+  liberarTJugador(borrar->jugador);
+  delete borrar;
+  borrar = NULL;
 }
