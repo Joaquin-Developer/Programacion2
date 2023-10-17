e979ce1f46e2f5cedac4f4f36a1d9b2e10d34886
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2023-10-17 03:10:32 -0300

    t-aplicaciones3-sumaPares

--- tarea3/src/aplicaciones.cpp
+++ tarea3/src/aplicaciones.cpp
@@ -1,9 +1,22 @@
 #include "../include/aplicaciones.h"
 
-TPilaJugador menoresQueElResto(TJugadoresLDE lista) {
+TPilaJugador menoresQueElResto(TJugadoresLDE lista)
+{
     return NULL;
 }
 
-bool sumaPares(nat k, TConjuntoIds c){
+bool sumaPares(nat k, TConjuntoIds c)
+{
+    for (nat i = 1; i <= cantMaxTConjuntoIds(c); i++)
+    {
+        nat id1 = i;
+        nat id2 = k - id1;
+
+        if (id1 == id2)
+            continue;
+
+        if (perteneceTConjuntoIds(id1, c) && perteneceTConjuntoIds(id2, c))
+            return true;
+    }
     return false;
 }
