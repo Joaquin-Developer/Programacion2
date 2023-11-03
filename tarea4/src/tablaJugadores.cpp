ffc19eac7966590b0f2c8d4e8256c5b8ef64afb5
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2023-11-02 21:23:28 -0300

    t-tabla2-insertarImprimir

--- tarea4/src/tablaJugadores.cpp
+++ tarea4/src/tablaJugadores.cpp
@@ -1,18 +1,8 @@
 #include "../include/tablaJugadores.h"
 
-struct rep_nodo_jugador;
-typedef struct rep_nodo_jugador *NDJugador;
-
-struct rep_nodo_jugador
-{
-    TFecha fecha;
-    TJugador jugador;
-    NDJugador sig;
-};
-
 struct rep_tablaJugadores
 {
-    NDJugador *tabla;
+    TJugadoresLDE *tabla;
     int tope;
 };
 
@@ -32,9 +22,9 @@ TTablaJugadores crearTTablaJugadores(int max)
 {
     TTablaJugadores tablaJugadores = new rep_tablaJugadores;
     tablaJugadores->tope = max;
-    tablaJugadores->tabla = new NDJugador[max];
+    tablaJugadores->tabla = new TJugadoresLDE[max];
     for (int i = 0; i < max; i++)
-        tablaJugadores->tabla[i] = NULL;
+        tablaJugadores->tabla[i] = crearTJugadoresLDE();
     return tablaJugadores;
 }
 
@@ -42,14 +32,7 @@ void liberarTTablaJugadores(TTablaJugadores &tabla)
 {
     for (int i = 0; i < tabla->tope; i++)
     {
-        while (tabla->tabla[i] != NULL)
-        {
-            NDJugador borrar = tabla->tabla[i];
-            liberarTFecha(borrar->fecha);
-            liberarTJugador(borrar->jugador);
-            tabla->tabla[i] = borrar->sig;
-            delete borrar;
-        }
+        liberarTJugadoresLDE(tabla->tabla[i]);
     }
     delete[] tabla->tabla;
     delete tabla;
@@ -59,38 +42,28 @@ void liberarTTablaJugadores(TTablaJugadores &tabla)
 void insertarJugadorEnTabla(TTablaJugadores &tabla, TJugador jugador, TFecha fecha)
 {
     int indice = funcionDeDispersion(nombreTJugador(jugador)) % tabla->tope;
-
-    // nodo a insertar:
-    NDJugador insertar = new rep_nodo_jugador;
-    insertar->jugador = jugador;
-    insertar->fecha = fecha;
-
-    // si la lista es nula, inserto al comienzo.
-    if (tabla->tabla[indice] == NULL)
-    {
-        insertar->sig = NULL;
-        tabla->tabla[indice] = insertar;
-    }
-    else
-    {
-        // sis no, inserto ordenado por fecha
-    }
+    insertarTJugadoresLDE(tabla->tabla[indice], jugador, fecha);
 }
 
 void eliminarJugadorDeTTablaJugadores(TTablaJugadores &tabla, const char nombre[100])
 {
+    // int indice = funcionDeDispersion(nombre) % tabla->tope;
 }
 
 bool perteneceATTablaJugadores(TTablaJugadores tabla, const char nombre[100])
 {
+    // int indice = funcionDeDispersion(nombre) % tabla->tope;
     return false;
 }
 
 TJugador obtenerJugadorDeTTablaJugadores(TTablaJugadores tabla, const char nombre[100])
 {
+    // int indice = funcionDeDispersion(nombre) % tabla->tope;
     return NULL;
 }
 
 void imprimirTTablaJugadores(TTablaJugadores tabla)
 {
+    for (int i = 0; i < tabla->tope; i++)
+        imprimirMayorAMenorTJugadoresLDE(tabla->tabla[i]);
 }
